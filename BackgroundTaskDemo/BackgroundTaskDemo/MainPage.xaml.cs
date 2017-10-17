2d6d1db957cd0e011353ac9656ecc9c939f376b3
MartinRep <repickym@gmail.com>
2017-10-17 11:02:59 +0100

    Background demo working

--- BackgroundTaskDemo/BackgroundTaskDemo/MainPage.xaml.cs
+++ BackgroundTaskDemo/BackgroundTaskDemo/MainPage.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.ApplicationModel.Background;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -20,11 +10,50 @@ namespace BackgroundTaskDemo
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    /// <summary> 
+    /// An empty page that can be used on its own or navigated to within a Frame. 
+    /// </summary>
+
     public sealed partial class MainPage : Page
     {
+
         public MainPage()
         {
             this.InitializeComponent();
         }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            var access = await BackgroundExecutionManager.RequestAccessAsync();
+
+            switch (access)
+            {
+                case BackgroundAccessStatus.Unspecified:
+                    break;
+                case BackgroundAccessStatus.AllowedMayUseActiveRealTimeConnectivity:
+                    break;
+                case BackgroundAccessStatus.AllowedWithAlwaysOnRealTimeConnectivity:
+                    break;
+                case BackgroundAccessStatus.Denied:
+                    break;
+                default:
+                    break;
+            }
+
+            var task = new BackgroundTaskBuilder
+            {
+                Name = "My Task",
+                TaskEntryPoint = typeof(BackgroundComponent.Class1).ToString()
+            };
+
+            var trigger = new ApplicationTrigger();
+            task.SetTrigger(trigger);
+
+            var condition = new SystemCondition(SystemConditionType.InternetAvailable);
+            task.Register();
+
+            await trigger.RequestAsync();
+        }
     }
+
 }
