51192cb841b364d352d87e243506929221e36cf1
MartinRep <repickym@gmail.com>
2017-10-17 10:20:19 +0100

    Check for Background Task added

--- BackgroundTaskDemo/BackgroundComponent/Class1.cs
+++ BackgroundTaskDemo/BackgroundComponent/Class1.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
 
 namespace BackgroundComponent
 {
-    public sealed class Class1
+    public sealed class Class1 : IBackgroundTask
     {
-    }
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            var access = await BackgroundExecutionManager.RequestAccessAsync();
+
+            switch (access)
+            {
+
+                case BackgroundAccessStatus.Unspecified:
+                    break;
+                case BackgroundAccessStatus.AllowedMayUseActiveRealTimeConnectivity:
+                    break;
+                case BackgroundAccessStatus.AllowedWithAlwaysOnRealTimeConnectivity:
+                    break;
+                case BackgroundAccessStatus.Denied:
+                    break;
+                default:
+                    break;
+            }
+
+            }
+        }
 }
